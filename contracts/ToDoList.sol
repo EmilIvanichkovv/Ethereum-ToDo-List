a4223c8faa64f60279259053784d3bb3c636937b
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2020-12-09 23:08:28 +0200

    Project Setup

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.5.0;
 contract ToDoList {
     uint public taskCount = 0;
 
+
 }
--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,7 @@ module.exports = {
     optimizer: {
       enabled: true,
       runs: 200
+      
     }
   }
 }
